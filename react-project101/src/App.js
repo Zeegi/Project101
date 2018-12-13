83198aaad003ade7fc0ca380b98dc3dcd5b52e94
Hariprasad.Sivanesan <Hariprasad.Sivanesan@gmail.com>
2018-12-13 00:04:50 -0500

    feat: displaying a list from the service

--- react-project101/src/App.js
+++ react-project101/src/App.js
@@ -3,23 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      users: [],
+      toDisplay: false,
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res=>res.json())
+      .then(data=>this.setState({users:data}));
+  }
+  
+
+  // fetch()
+
   render() {
+    const userList = this.state.users.map((user =>(
+      <div key = {user.id}>
+        <h3>{user.title}</h3>
+        <p>{user.body}</p>
+      </div>
+    )))
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Displaying a list of items</h1>
+            {userList}
       </div>
     );
   }
